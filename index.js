9beb43e98fdbcee601ab3562f5d19f4f2a729163
RoieOnn <onn.roie@gmail.com>
2017-05-26 12:04:36 +0300

    I love matan so much

--- index.js
+++ index.js
@@ -7,9 +7,38 @@ const port = process.env.PORT || 8080;
 
 //////////////////////////////////////////////////////////////////////////////////////
 
+function saveUser(id, lon, lat){
+	MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+   let collection = db.collection('users');
+   collection.insertMany([
+      {username: id, longitude:lon, latitude:lat}
+   ], (err, results) => {
+          db.close();
+          if (err) {
+              console.error(err);
+              return;
+            }
+          console.log(`Completed successfully, inserted ${results.insertedCount} documents`);
+      });
+});
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+app.get("/", function(req, res){
+	console.log(req, res);
+	res.send("Hello");
+	res.end();
+});
+
+
 app.get("/updateUser", function(req, res){
 	// console.log(req.body, res);
-	console.log(req.query.id);
+	console.log(req.query);
 	// saveUser(req.query.id, req.query.lon, req.query.lat);
 	res.send("updated");
 	res.end();
