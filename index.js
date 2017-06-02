c5fdf8398abd4c3994a91577aa5e7caac44d740d
RoieOnn <onn.roie@gmail.com>
2017-06-02 12:37:27 +0300

    matching and updating DB

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const MongoClient = require('mongodb').MongoClient;
 const MONGO_URL = "mongodb://roieonn:romer10@ds147821.mlab.com:47821/gameonv1";
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(bodyParser.json());
 
 //////////////////////////////////////////////////////////////////////////////////////
-
-function saveUser(id, lon, lat){
+	
+function saveUser(id, sport, lvl, lon, lat){
 	MongoClient.connect(MONGO_URL, (err, db) => {
     if (err) {
         console.error(err);
@@ -15,7 +17,7 @@ function saveUser(id, lon, lat){
     }
    let collection = db.collection('users');
    collection.insertMany([
-      {username: id, longitude:lon, latitude:lat}
+      {username: id, sport: sport, level: lvl, longitude:lon, latitude:lat}
    ], (err, results) => {
    			console.log(err, results);
           db.close();
@@ -27,7 +29,26 @@ function saveUser(id, lon, lat){
       });
 });
 }
-function userMatch(sport, lvl, clock){
+function findOpponent(sport, lvl, clock, cb){
+	MongoClient.connect(MONGO_URL, (err, db) => {
+	    if (err) {
+	        console.error(err);
+
+	        return;
+	   }
+
+	   let collection = db.collection('users');
+	   collection.find({sport: sport, lvl: parseInt(lvl)}).toArray((err, result) => {
+	   		if (err) {
+	   			console.error(err);
+	   		}
+	   		cb(null, result)
+	   }); 
+
+   	});
+}
+
+function ready(sport, lvl, clock){
 	MongoClient.connect(MONGO_URL, (err, db) => {
     if (err) {
         console.error(err);
@@ -56,29 +77,40 @@ app.get("/", function(req, res){
 	res.end();
 });
 
-app.get("/userMatch", function(req, res){
+app.get("/findOpponent", function(req, res){
+	findOpponent(req.query.sport, req.query.lvl, req.query.clock, (err, result) => {
+		if (err) return res.status(500).json({err: err.message});
+        return res.json(result);
+    });	
+});
+
+
+app.post("/findOpponent", function(req, res){
 	console.log(req.query);
 	userMatch(req.query.sport, req.query.lvl, req.query.clock);
-	res.send("looking for match");
+	res.send("Ready");
 	res.end();
 });
 
 app.get("/updateUser", function(req, res){
-	// console.log(req.body, res);
 	console.log(req.query);
-	saveUser(req.query.id, req.query.lon, req.query.lat);
+	saveUser(req.query.id, req.query.sport, req.query.lvl, req.query.lon, req.query.lat);
 	res.send("updated");
 	res.end();
 });
 
 app.post('/updateUser', (req, res) => {
-    console.dir(req.body);
     let username = req.body.username || DEFAULT_USERNAME;
+    let sport = req.body.sport || DEFAULY_SPORT;
+    let level = req.body.level || DEFAULT_LEVEL;
     let longitude = req.body.longitude || DEFAULT_LONGITUDE;
     let latitude = req.body.latitude || DEFAULT_LATITUDE;
-    console.log("Got POST request to /updateUser, using username=" + username + 
-    										   ", longitude=" + longitude + 
-    										   ", latitude=" + latitude);
+	
+	saveUser(username, sport, level, longitude, latitude);
+
+	res.send(JSON.stringify(req.body));
+	res.end();
 });
 
+console.log(port);
 app.listen(port);
